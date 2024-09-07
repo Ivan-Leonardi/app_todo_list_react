e4d7b36850bc14e0e02bdf9d0041ce559fe04e55
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-09-07 11:09:26 -0300

    feat: add validation form lib yup

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -5,30 +5,58 @@ import { BackgroundIMG, Container, Form } from "./styles";
 import { UserRound, Mail, LockKeyhole } from "lucide-react";
 import { Link } from "react-router-dom";
 import { api } from "../../services/api";
+import * as yup from "yup";
 
 export function Register() {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleRegister() {    
-    if (!firstName || !email || !password) {
-      return alert("Todos os campos são obrigatórios!")
-    }
+  const schemaRegister = yup.object().shape({
+    firstName: yup
+      .string()
+      .min(3, "O nome deve conter pelo menos 3 caracteres")
+      .required("O nome é obrigatório"),
+    email: yup
+      .string()
+      .email("Insira um email válido")
+      .required("Email obrigatório"),
+    password: yup
+      .string()
+      .min(6, "A senha deve conter pelo menos 6 caracteres")
+      .required("Senha obrigatória"),
+  });
 
-    api.post("/users", {
+  async function handleRegister() {
+    const data = {
       firstName,
       email,
       password,
-    }).then(() => {
-      alert("Usuário cadatrado com sucesso!");
-    }).catch((error) => {
-      if(error.response) {
-        alert(error.response.data.message);
+    };
+
+    try {
+      await schemaRegister.validate(data);
+
+      await api
+        .post("/users", {
+          firstName,
+          email,
+          password,
+        })
+        .then(() => {
+          alert("Usuário cadatrado com sucesso!");
+
+          setFirstName("");
+          setEmail("");
+          setPassword("");
+        });
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        alert(error.errors);
       } else {
-        alert("Erro ao realizar registro")
+        alert("Erro ao realizar registro");
       }
-    })
+    }
   }
 
   return (
@@ -41,29 +69,26 @@ export function Register() {
         <p>Um jeito prático e simples de salvar suas tarefas do dia a dia</p>
         <span>Crie sua conta!</span>
 
-        <Input 
-        placeholder="Nome" 
-        type="text" 
-        icon={UserRound}
-        onChange={e => setFirstName(e.target.value)} 
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={UserRound}
+          onChange={(e) => setFirstName(e.target.value)}
         />
-        <Input 
-        placeholder="Email" 
-        type="text" 
-        icon={Mail}
-        onChange={e => setEmail(e.target.value)} 
+        <Input
+          placeholder="Email"
+          type="text"
+          icon={Mail}
+          onChange={(e) => setEmail(e.target.value)}
         />
-        <Input 
-        placeholder="Senha" 
-        type="password" 
-        icon={LockKeyhole} 
-        onChange={e => setPassword(e.target.value)}
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={LockKeyhole}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button 
-        title="Enviar" 
-        onClick={handleRegister}
-        />
+        <Button title="Enviar" onClick={handleRegister} />
 
         <p>
           Já possui uma conta? <Link to={"/"}>Faça seu login</Link>
