9909e355bc4b8c166eed090a7cad650bbc740bff
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-09-18 18:58:18 -0300

    feat: add lib date-fns to ListTasks

--- src/components/ListTask/index.jsx
+++ src/components/ListTask/index.jsx
@@ -1,59 +1,62 @@
 import { useState } from "react";
 import { TaskContainer, TaskItem } from "./styles";
 import { Trash, Pencil } from "lucide-react";
+import { format } from "date-fns";
 
 import { api } from "../../services/api";
 import { CardEditTask } from "../CardEditTask";
 
 export function ListTasks({ tasks, setTasks }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedTask, setSelectedTask] = useState(null);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedTask, setSelectedTask] = useState(null);
 
-  const openModal = (task) => {
-    setSelectedTask(task);
-    setIsModalOpen(true);
-  };
+    const openModal = (task) => {
+        setSelectedTask(task);
+        setIsModalOpen(true);
+    };
 
-  const closeModal = () => setIsModalOpen(false);
+    const closeModal = () => setIsModalOpen(false);
 
-  async function deleteTask(taskId) {
-    try {
-      await api.delete(`/tasks/${taskId}`);
-      setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-    } catch (error) {
-      console.error(error);
+    async function deleteTask(taskId) {
+        try {
+            await api.delete(`/tasks/${taskId}`);
+            setTasks((prevTasks) =>
+                prevTasks.filter((task) => task.id !== taskId)
+            );
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  return (
-    <TaskContainer>
-      {tasks && tasks.length > 0 ? (
-        tasks.map((task) => (
-          <TaskItem key={task.id} status={task.status}>
-            <section>
-              <div>
-                <h4>{task.title}</h4>
-                <p>{task.description}</p>
-                <span>Prazo: {task.due_date}</span>
-              </div>
-              <div>
-                <Pencil onClick={() => openModal(task)} />
-                <Trash onClick={() => deleteTask(task.id)} />
-              </div>
-            </section>
-          </TaskItem>
-        ))
-      ) : (
-        <p>Nenhuma tarefa encontrada!</p>
-      )}
-      {selectedTask && (
-        <CardEditTask
-          isOpen={isModalOpen}
-          onClose={closeModal}
-          task={selectedTask}
-          setTasks={setTasks}
-        />
-      )}
-    </TaskContainer>
-  );
+    return (
+        <TaskContainer>
+            {tasks && tasks.length > 0 ? (
+                tasks.map((task) => (
+                    <TaskItem key={task.id} status={task.status}>
+                        <section>
+                            <div>
+                                <h4>{task.title}</h4>
+                                <p>{task.description}</p>
+                                <span>Prazo: {format(new Date(task.due_date), "dd-MM-yyyy")}</span>
+                            </div>
+                            <div>
+                                <Pencil onClick={() => openModal(task)} />
+                                <Trash onClick={() => deleteTask(task.id)} />
+                            </div>
+                        </section>
+                    </TaskItem>
+                ))
+            ) : (
+                <p>Nenhuma tarefa encontrada!</p>
+            )}
+            {selectedTask && (
+                <CardEditTask
+                    isOpen={isModalOpen}
+                    onClose={closeModal}
+                    task={selectedTask}
+                    setTasks={setTasks}
+                />
+            )}
+        </TaskContainer>
+    );
 }
